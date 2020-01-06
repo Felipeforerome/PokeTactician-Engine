c1bdd9d798bf6b601e47f41c3fdccee516330df6
Felipe Forero <felipe.forerome@gmail.com>
2020-01-05 22:02:58 -0500

    Main.py parsed pokeapi (local)

--- main.py
+++ main.py
@@ -1,66 +1,35 @@
-typeChart = []
+#Query Database
 
-class Pokemon:
-    def __init__(self, name, hp, att, deff, spatt, spdeff, spe, type1, type2 = ""):
-        self.name = name
-        self.hp = hp
-        self.att = att
-        self.deff = deff
-        self.spatt = spatt
-        self.spdeff = spdeff
-        self.spe = spe
-        self.learnedMoves = []
-        self.moves = []
-        self.type1 = type1
-        self.type2 = type2
+import requests
 
-    def addMove(self, move):
-        self.moves += move
+from models.Pokemon import Pokemon
+import pickle
 
-    def expectedAttacks(self):
-        moves = self.moves
-        expectedAttacks = []
-        for move in moves:
-            moveName = move.Name
-            moveType = move.type
-            movePower = move.power
-            moveDamageClass = move.damageClass
-            moveAccuracy = move.accuracy
-            stab = 1.5 if (moveType == self.type1 or moveType == self.type2) else 1
-            split = self.att if (moveDamageClass == "physical") else self.spatt
-            expectedPower = (stab * movePower) * split * moveAccuracy
-            expectedAttacks += (moveName, expectedPower)
-        return expectedAttacks
+i = 1
+movesTotal = 0
+Pokemons = []
+lastExisted = True
 
-    def learnMove(self, move, place=None):
-        if(place==None):
-            numberMoves = self.learnedMoves.__len__()
-            if(numberMoves<4):
-                self.learnedMoves[place] = move
-            else:
-                raise Exception("Missing Place Argument, This Pokemon Already Knows 4 Moves")
+while (lastExisted):
+    pokemonpreJSON = requests.get('http://127.0.0.1:8000/api/v2/pokemon/'+str(i))
+    is404 = pokemonpreJSON.status_code == 404
+    if(not is404):
+        pokemonJSON = pokemonpreJSON.json()
+        tempPoke = Pokemon(
+            pokemonJSON['name'],
+            pokemonJSON['stats'][0]['base_stat'],
+            pokemonJSON['stats'][1]['base_stat'],
+            pokemonJSON['stats'][2]['base_stat'],
+            pokemonJSON['stats'][3]['base_stat'],
+            pokemonJSON['stats'][4]['base_stat'],
+            pokemonJSON['stats'][5]['base_stat'],
+            pokemonJSON['types'][0]['type'].get('name'),
+            type2= None if len(pokemonJSON['types'])<2 else pokemonJSON['types'][1]['type'].get('name')
+        )
+        Pokemons.append(tempPoke)
+        movesTotal += pokemonJSON['moves'].__len__()
+        i+=1
+    else:
+        lastExisted = False
 
-    def currentPower(self):
-        currentPower = 0
-        if self.learnedMoves.__len__() <1:
-            raise Exception("This Pokemon Needs To Learn A Move")
-        else:
-            for learnedMove in self.learnedMoves:
-                moveType = learnedMove.type
-                movePower = learnedMove.power
-                moveDamageClass = learnedMove.damageClass
-                moveAccuracy = learnedMove.accuracy
-                stab = 1.5 if (moveType == self.type1 or moveType == self.type2) else 1
-                split = self.att if (moveDamageClass == "physical") else self.spatt
-                expectedPower = (stab * movePower) * split * moveAccuracy
-                currentPower += expectedPower
-            return currentPower
-
-
-class Move:
-    def __init__(self, name, type, damageClass, power, accuracy):
-        self.name = name
-        self.type = type
-        self.damageClass = damageClass
-        self.power = power
-        self.accuracy = accuracy/100
+print(movesTotal/i)
