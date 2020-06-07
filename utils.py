417d46aaf9f7abb799687265cfeefd90c1b275b2
Felipe Forero <felipe.forerome@gmail.com>
2020-06-06 19:54:35 -0500

    Utility funcitons to count knowable moves by pokemon

---
+++ utils.py
@@ -0,0 +1,43 @@
+def moveExactCounter(PokemonsList):
+    """
+    Count the number of pokemon that have X number of knowable attacks (until 36)
+    :param PokemonsList: List of Pokemon to run through
+    :return: Array containing the amount of pokemons that can learn X amount of moves
+    """
+    numberofmoves = []
+    i_s = []
+    i = 0
+    while(i < 36):
+        quantPok = 0
+        for pok in PokemonsList:
+            quantMoves = pok.knowableMoves.__len__()
+            if(quantMoves == i):
+                quantPok = quantPok + 1
+        numberofmoves.append(quantPok)
+        i_s.append(i)
+        i = i + 1
+    print(i_s)
+    return numberofmoves
+
+def moveTotalCounter(PokemonsList):
+    """
+    Count the number of pokemon that have at least X number of knowable attacks (until 36)
+    :param PokemonsList: List of Pokemon to run through
+    :return: Array containing the amount of pokemon that can learn at least X amount of moves
+    """
+    numberofmoves = []
+    i_s = []
+    tempmove = None
+    i = 0
+    while(tempmove != 1):
+        quantPok = 0
+        for pok in PokemonsList:
+            quantMoves = pok.knowableMoves.__len__()
+            if(quantMoves >= i):
+                quantPok = quantPok + 1
+        numberofmoves.append(quantPok)
+        i_s.append(i)
+        i = i + 1
+        tempmove = quantPok
+    print(i_s)
+    return numberofmoves
