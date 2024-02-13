503053dd8df5565ef1c7306fed97b70cd10e9b20
Felipe Forero <felipe.forerome@gmail.com>
2024-02-13 23:48:36 +0100

    Toogle Obj Func with Check List Picker

--- dash_app/callbacks.py
+++ dash_app/callbacks.py
@@ -1,4 +1,4 @@
-from dash import Input, Output, callback
+from dash import Input, Output, callback, State
 import sys
 
 sys.path.append(sys.path[0] + "/..")
@@ -16,30 +16,35 @@ import time
 @callback(
     Output("team-output", "children"),
     Output("time-to-calc", "children"),
-    [Input("suggest-team-btn", "n_clicks")],
+    [Input("suggest-team-btn", "n_clicks"), State("objective-funcs", "value")],
 )
-def update_output(n):
+def update_output(n, objFuncsParam):
     if n is None:
         return "Click the button to suggest a team.", ""
     else:
         start = time.time()
+        objectiveFuncs = []
         attackObjFun = lambda team: attack_obj_fun(team)
         teamCoverageFun = lambda team: team_coverage_fun(team)
         selfCoverageFun = lambda team: self_coverage_fun(team)
-        objectiveFuncs = [
-            (attackObjFun, Q, rho),
-        ]
+        if "1" in objFuncsParam:
+            objectiveFuncs.append((attackObjFun, Q, 0.1))
+        if "2" in objFuncsParam:
+            objectiveFuncs.append((teamCoverageFun, Q, rho))
+        if "3" in objFuncsParam:
+            objectiveFuncs.append((selfCoverageFun, Q, rho))
+        print(objectiveFuncs)
         mCol = MOACO(
             ColonyGPT,
-            600,
+            400,
             objectiveFuncs,
             pokPreFilter,
             alpha,
             beta,
         )
-        mCol.optimize(iters=50, time_limit=None)
+        mCol.optimize(iters=20, time_limit=None)
         team = mCol.getSolnTeamNames()
         return (
             f"Suggested Team: {', '.join(team)}",
-            f"Time to compute: {time.time()-start}",
+            f"Time to compute: {time.time()-start} - Objective Value: {mCol.getObjTeamValue()}",
         )
--- dash_app/layouts.py
+++ dash_app/layouts.py
@@ -4,6 +4,14 @@ from dash import html, dcc
 layout = html.Div(
     [
         html.H1("Pokémon Team Optimizer"),
+        dcc.Checklist(
+            options={1: "Attack", 2: "Team Coverage", 3: "Self Coverage"},
+            value=[
+                "Attack",
+            ],
+            inline=True,
+            id="objective-funcs",
+        ),
         html.Button("Suggest Team", id="suggest-team-btn"),
         dcc.Loading(
             id="loading-output",
