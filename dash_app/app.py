49756dfc484416e115d2f32bcea7d61bac95521e
Felipe Forero <felipe.forerome@gmail.com>
2024-03-10 13:58:45 +0100

    Add production server, and change port

--- dash_app/app.py
+++ dash_app/app.py
@@ -2,16 +2,19 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 from layouts import layout
 import callbacks  # This imports the callbacks to register them with the app
+from decouple import config
 
 # Initialize the Dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
+app.title = "PokéTactician"
 server = app.server
 
 app.layout = layout
 
 if __name__ == "__main__":
-    try:
-        app.run(debug=True, host="0.0.0.0", port=8050)
-    except Exception as e:
-        print(e)
+    if config("DEBUG", False, cast=bool):
+        app.run(debug=True, host="0.0.0.0", port=8080)
+    else:
+        from waitress import serve
+
+        serve(server, host="0.0.0.0", port=8080)
