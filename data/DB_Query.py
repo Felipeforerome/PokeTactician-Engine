5165b3044a9fb5eb6a0ffddd203c8b271e05265e
Felipe Forero <felipe.forerome@gmail.com>
2024-05-26 23:11:50 +0200

    Query DB from GraphQL, update datasets, craete hasura_metadata

--- data/DB_Query.py
+++ data/DB_Query.py
@@ -1,92 +1,149 @@
 # Query Database
+# To run this script, you need to have the PokeAPI running on your local machine. To do this do the following steps:
+# - Clone the repository from
+# - If you already had the repo, delete the volumes and images of the containers
+# - Run the docker configuration from the README
+# - Go into Hasura and import all tables and foreign keys (this can take a bit)
+# - Import the hasura metadata from the metadata file in the repo
 
-import requests
 import sys
 
+import requests
+
 sys.path.append(sys.path[0] + "/..")
-from poketactician.models.Pokemon import Pokemon
-from poketactician.models.Move import Move
+import json
 import pickle
 import re
 import time
-import json
 
-tic = time.time()
-i = 1
-j = 1
-movesTotal = 0
-Pokemons = []
-Moves = {}
-lastMove = True
-recalculateMoves = False
-print("Started: " + str(tic))
-if recalculateMoves:
-    while lastMove:
-        movepreJSON = requests.get("http://localhost:80/api/v2/move/" + str(j))
-        is404 = movepreJSON.status_code == 404
-        if not is404:
-            moveJSON = movepreJSON.json()
-            tempMove = Move(
-                str(j),
-                moveJSON["name"],
-                moveJSON["type"]["name"],
-                moveJSON["damage_class"]["name"],
-                moveJSON["power"],
-                moveJSON["accuracy"],
-                moveJSON["pp"],
-                moveJSON["priority"],
-            )
-            Moves[str(j)] = tempMove
-            j += 1
-        else:
-            lastMove = False
-    print("Finished Moves")
-    movesDict = {k: v.serialize() for k, v in Moves.items()}
-    with open("data/move_data.json", "w") as f:
-        json.dump(movesDict, f, indent=4)
-else:
-    with open("data/move_data.json", "r") as f:
-        data = json.load(f)
-        Moves = {k: Move.from_json(pokemon_data) for k, pokemon_data in data.items()}
+from poketactician.models.Move import Move
+from poketactician.models.Pokemon import Pokemon
 
 
-lastExisted = True
-while lastExisted:
-    pokemonpreJSON = requests.get("http://localhost:80/api/v2/pokemon/" + str(i))
-    is404 = pokemonpreJSON.status_code == 404
-    if not is404:
-        pokemonJSON = pokemonpreJSON.json()
-        tempPoke = Pokemon(
-            pokemonJSON["id"],
-            pokemonJSON["name"],
-            pokemonJSON["stats"][0]["base_stat"],
-            pokemonJSON["stats"][1]["base_stat"],
-            pokemonJSON["stats"][2]["base_stat"],
-            pokemonJSON["stats"][3]["base_stat"],
-            pokemonJSON["stats"][4]["base_stat"],
-            pokemonJSON["stats"][5]["base_stat"],
-            pokemonJSON["types"][0]["type"].get("name"),
-            type2=(
-                None
-                if len(pokemonJSON["types"]) < 2
-                else pokemonJSON["types"][1]["type"].get("name")
-            ),
+def build_data(recalculateMoves):
+    """
+    Builds the data for the PokemonOpti application.
+
+    Args:
+        recalculateMoves (bool): Flag indicating whether to recalculate the moves or use existing data.
+
+    Returns:
+        None
+    """
+    tic = time.time()
+    i = 1
+    j = 1
+    movesTotal = 0
+    Pokemons = []
+    # Initialize Moves with a no-move for cases where the pokemon has no moves
+    Moves = {
+        "0": Move.from_json(
+            {
+                "id": "0",
+                "name": "no-move",
+                "type": "Normal",
+                "damageClass": "physical",
+                "power": 1,
+                "accuracy": 0,
+                "pp": 0,
+                "priority": 0,
+            }
         )
-        availableMoves = pokemonJSON["moves"]
-        for availableMove in availableMoves:
-            number = re.findall(r"[0-9]{1,}(?=/$)", availableMove["move"]["url"])[0]
-            appendMove = Moves[str(number)]
-            tempPoke.addKnowableMove(appendMove)
-
-        Pokemons.append(tempPoke)
-        movesTotal += pokemonJSON["moves"].__len__()
-        i += 1
-        if i % 100 == 0:
-            print(str(i) + ": " + pokemonJSON["name"])
+    }
+    lastMove = True
+    print("Started: " + time.strftime("%H:%M:%S", time.gmtime(tic + 7200)))
+    if recalculateMoves:
+        while lastMove:
+            movepreJSON = requests.get(
+                "http://localhost:8080/api/rest/moves/" + str(j),
+                headers={"x-hasura-admin-secret": "pokemon"},
+            )
+            moveJSON = movepreJSON.json()["moves"]
+            emptyListResp = len(moveJSON) == 0
+            if not emptyListResp:
+                moveJSON = moveJSON[0]
+                tempMove = Move(
+                    str(j),
+                    moveJSON["name"],
+                    moveJSON["type"]["name"],
+                    moveJSON["damage_class"]["name"],
+                    moveJSON["power"],
+                    moveJSON["accuracy"],
+                    moveJSON["pp"],
+                    moveJSON["priority"],
+                )
+                Moves[str(j)] = tempMove
+                j += 1
+                if j % 100 == 0:
+                    print(str(j) + ": " + moveJSON["name"])
+            else:
+                lastMove = False
+        movesDict = {k: v.serialize() for k, v in Moves.items()}
+        with open("data/move_data.json", "w") as f:
+            json.dump(movesDict, f, indent=4)
     else:
-        lastExisted = False
+        with open("data/move_data.json", "r") as f:
+            data = json.load(f)
+            Moves = {
+                k: Move.from_json(pokemon_data) for k, pokemon_data in data.items()
+            }
 
-serialized_pokemon_list = [pokemon.serialize() for pokemon in Pokemons]
-with open("data/pokemon_data.json", "w") as f:
-    json.dump(serialized_pokemon_list, f, indent=4)
-toc = time.time()
+    print("Finished Moves")
+    lastExisted = True
+    normalForms = True
+    print("Starting Pokemons")
+    while lastExisted:
+        pokemonpreJSON = requests.get(
+            "http://localhost:8080/api/rest/pokemon/" + str(i),
+            headers={"x-hasura-admin-secret": "pokemon"},
+        )
+        pokemonJSON = pokemonpreJSON.json()["pokemon"]
+        emptyListResp = len(pokemonJSON) == 0
+        if not emptyListResp:
+            pokemonJSON = pokemonJSON[0]
+            tempPoke = Pokemon(
+                pokemonJSON["id"],
+                pokemonJSON["name"],
+                pokemonJSON["stats"][0]["base_stat"],
+                pokemonJSON["stats"][1]["base_stat"],
+                pokemonJSON["stats"][2]["base_stat"],
+                pokemonJSON["stats"][3]["base_stat"],
+                pokemonJSON["stats"][4]["base_stat"],
+                pokemonJSON["stats"][5]["base_stat"],
+                pokemonJSON["types"][0]["type"].get("name"),
+                (
+                    None
+                    if len(pokemonJSON["types"]) < 2
+                    else pokemonJSON["types"][1]["type"].get("name")
+                ),
+                bool(pokemonJSON["legend"]["is_mythical"]),
+                bool(pokemonJSON["legend"]["is_legendary"]),
+                bool(pokemonJSON["formAbout"][0]["is_battle_only"]),
+                bool(pokemonJSON["formAbout"][0]["is_mega"]),
+            )
+            availableMoves = pokemonJSON["moves"]
+            for availableMove in availableMoves:
+                number = availableMove["move"]["id"]
+                appendMove = Moves[str(number)]
+                tempPoke.addKnowableMove(appendMove)
+
+            Pokemons.append(tempPoke)
+            movesTotal += pokemonJSON["moves"].__len__()
+            i += 1
+            if i % 100 == 0:
+                print(str(i) + ": " + pokemonJSON["name"])
+        else:
+            if not normalForms:
+                lastExisted = False
+            normalForms = False
+            i = 10001
+
+    serialized_pokemon_list = [pokemon.serialize() for pokemon in Pokemons]
+    with open("data/pokemon_data.json", "w") as f:
+        json.dump(serialized_pokemon_list, f, indent=4)
+    toc = time.time()
+    elapsed_time = toc - tic
+    print("Elapsed Time: {:.2f} seconds".format(elapsed_time))
+
+
+build_data(True)
